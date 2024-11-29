e3e72220f501b868a692a88bc2f36fb5b7a0e854
amisg <aamithsg18@gmail.com>
2024-11-29 21:24:24 +0530

    option button mobile view

--- src/components/QuestionCard.js
+++ src/components/QuestionCard.js
@@ -1,6 +1,19 @@
+import { useState, useEffect } from "react";
 import "../App.css";
 
 function QuestionCard({ question, questionNumber, onAnswer }) {
+	const [selectedOptionIndex, setSelectedOptionIndex] = useState(null);
+
+	// Reset selected option whenever the question changes
+	useEffect(() => {
+		setSelectedOptionIndex(null);
+	}, [question]);
+
+	const handleOptionClick = (index, score, text) => {
+		setSelectedOptionIndex(index); // Set the current option as selected
+		onAnswer(score, text); // Trigger the callback
+	};
+
 	return (
 		<div className="question-card">
 			<h2>
@@ -19,12 +32,14 @@ function QuestionCard({ question, questionNumber, onAnswer }) {
 				{question.options.map((option, index) => (
 					<button
 						key={index}
-						className="option-button"
-						onClick={() => onAnswer(option.score, option.text)}>
+						className={`option-button ${
+							selectedOptionIndex === index ? "selected" : ""
+						}`}
+						onClick={() => handleOptionClick(index, option.score, option.text)}>
 						<div className="option-content">
 							<div>
 								<p>{option.text}</p>
-								<p>Impact:{option.impact}</p>
+								<p>Impact: {option.impact}</p>
 							</div>
 						</div>
 					</button>
